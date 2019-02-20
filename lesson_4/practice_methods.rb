a3c5476dbac0374bf0be61f178ddd165f165fcd9
Melissa Harvey <melissawharvey@gmail.com>
2019-02-20 08:03:05 -0600

    Work on method exercises lesson 4

---
+++ lesson_4/practice_methods.rb
@@ -0,0 +1,34 @@
+# 1 
+# => [1, 2, 3]
+# 'hi' is the return value of the block and is "truthy"
+
+# 2
+# Looks for truthiness
+
+# 3
+# => [1, 2, 3]
+
+# 4
+# => { "a" => "ant", "b" => "bear", "c" => "cat"}
+
+# 5
+# => [:a, "ant"]
+
+
+# 6
+# => 11
+# returns last item "caterpillar" and counts size of string 11
+
+# 7
+# => 1
+
+# 8
+# takes n element(s) and returns them
+# not destructive because it returns new array
+# => [1, 2]
+
+# 9 
+# => [nil, "bear"]
+
+# 10
+# => [1, nil, nil]
